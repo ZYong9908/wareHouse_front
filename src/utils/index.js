092904245db8fccc8eec11474411e7bfaca192f9
zouyong <zyong9908@qq.com>
2023-05-15 17:26:24 +0800

    opt: opt

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,8 @@
  * Created by PanJiaChen on 16/11/18.
  */
 
+import moment from 'moment/moment'
+
 /**
  * Parse the time to string
  * @param {(Object|string|number)} time
@@ -56,42 +58,9 @@ export function parseTime(time, cFormat) {
  * @param {string} option
  * @returns {string}
  */
-export function formatTime(time, option) {
-  if (('' + time).length === 10) {
-    time = parseInt(time) * 1000
-  } else {
-    time = +time
-  }
-  const d = new Date(time)
-  const now = Date.now()
-
-  const diff = (now - d) / 1000
-
-  if (diff < 30) {
-    return '刚刚'
-  } else if (diff < 3600) {
-    // less 1 hour
-    return Math.ceil(diff / 60) + '分钟前'
-  } else if (diff < 3600 * 24) {
-    return Math.ceil(diff / 3600) + '小时前'
-  } else if (diff < 3600 * 24 * 2) {
-    return '1天前'
-  }
-  if (option) {
-    return parseTime(time, option)
-  } else {
-    return (
-      d.getMonth() +
-      1 +
-      '月' +
-      d.getDate() +
-      '日' +
-      d.getHours() +
-      '时' +
-      d.getMinutes() +
-      '分'
-    )
-  }
+export function formatTime(time){
+  if (!time) return ''
+  return moment(time).format('YYYY-MM-DD HH:mm:ss')
 }
 
 /**
